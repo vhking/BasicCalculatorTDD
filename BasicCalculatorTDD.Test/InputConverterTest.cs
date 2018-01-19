5904205b9258cabcefaf398182efcd7b76a9a3d4
vhking <vidarheltne@hotmail.no>
2018-01-19 14:37:29 +0100

    converting invalid input test

--- BasicCalculatorTDD.Test/InputConverterTest.cs
+++ BasicCalculatorTDD.Test/InputConverterTest.cs
@@ -1,3 +1,4 @@
+using System;
 using NUnit.Framework;
 
 namespace BasicCalculatorTDD.Test
@@ -5,13 +6,21 @@ namespace BasicCalculatorTDD.Test
     public class InputConverterTest
     {
         private readonly InputConverter _inputConverter = new InputConverter();  
-              
+
         [Test]
         public void ConvertInputToNumeric_ConvertsValidInputIntoDouble_ReturnsTrue()
         {
             string input = "10";
             double convertedInput = _inputConverter.ConvertInputToNumeric(input);
             Assert.That(10,Is.EqualTo(convertedInput) );
+        }
+
+        [Test]
+        public void ConvertInputToNumeric_ConvertInvalidInputIntoDouble_ReturnsFalse()
+        {
+            // invalid character input
+            string input = "^";
+            Assert.Throws<ArgumentException>(() => _inputConverter.ConvertInputToNumeric(input));
         }
         
     }
